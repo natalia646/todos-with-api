8f0bd2af3a463f3ad3e33f1e6ba260cd20538889
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-31 17:24:49 +0200

    fix prettier bugs

--- src/types/ErrorMessage.ts
+++ src/types/ErrorMessage.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 export enum ErrorMessage {
   Default = '',
   EmptyTitle = 'Title should not be empty',
--- src/types/Status.ts
+++ src/types/Status.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 export enum Status {
   All = 'All',
   Active = 'Active',
